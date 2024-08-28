af647fd734bd57eb8adcff6fcc911fd599688776
Daniel Ribeiro <daniel.junior@gmail.com>
2024-08-28 10:52:55 -0300

    change from docker to podman

--- lab-jaeger-istio-nodejs/service-a/build-push.sh
+++ lab-jaeger-istio-nodejs/service-a/build-push.sh
@@ -3,5 +3,9 @@
 #docker build . -t default-route-openshift-image-registry.apps-crc.testing/default/service-a-nodejs-istio
 #docker push default-route-openshift-image-registry.apps-crc.testing/default/service-a-nodejs-istio
 
-docker build . -t csantanapr/service-a-nodejs-istio
-docker push csantanapr/service-a-nodejs-istio
+#docker build . -t csantanapr/service-a-nodejs-istio
+#docker push csantanapr/service-a-nodejs-istio
+
+# Versão podman
+podman build -t danielrpgj/service-a-nodejs .
+podman tag my-nginx-image registry.example.com/my-nginx-image:v1
--- lab-jaeger-istio-nodejs/service-b/build-push.sh
+++ lab-jaeger-istio-nodejs/service-b/build-push.sh
@@ -3,5 +3,9 @@
 #docker build . -t default-route-openshift-image-registry.apps-crc.testing/default/service-b-nodejs-istio
 #docker push default-route-openshift-image-registry.apps-crc.testing/default/service-b-nodejs-istio
 
-docker build . -t csantanapr/service-b-nodejs-istio
-docker push csantanapr/service-b-nodejs-istio
+#docker build . -t csantanapr/service-b-nodejs-istio
+#docker push csantanapr/service-b-nodejs-istio
+
+# Usando podman para tutorial 
+podman build -t service-b-nodejs .
+podman tag localhost/service-b-nodejs registry.example.com/service-b-nodejs:v1
